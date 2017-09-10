2c3c199ece33c442f78a536db6d32112ada5e85d
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2017-09-10 15:35:47 +0300

    chore: update devDependencies

--- src/utils.js
+++ src/utils.js
@@ -15,24 +15,21 @@ module.exports = (config) => {
       tileliveCache.info(uri, (error, metadata) => {
         if (error) reject(error);
         resolve(metadata);
-      })
-    );
+      }));
 
   const load = uri =>
     new Promise((resolve, reject) =>
       tileliveCache.load(uri, (error, source) => {
         if (error) reject(error);
         resolve(source);
-      })
-    );
+      }));
 
   const getTile = (source, z, x, y) =>
     new Promise((resolve, reject) =>
       source.getTile(z, x, y, (error, tile, headers) => {
         if (error) reject(error);
         resolve({ headers, tile });
-      })
-    );
+      }));
 
   return { info, load, getTile };
 };
